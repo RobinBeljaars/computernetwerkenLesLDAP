cb1b9d5e997b6a315dd6c02844c617afb555d960
RobinBeljaars <rp.beljaars@student.avans.nl>
2020-04-24 09:20:36 +0200

    startup project

---
+++ les 4 computernetwerken/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace les_4_computernetwerken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string LDAP_PATH = "L";
+        }
+    }
+}
